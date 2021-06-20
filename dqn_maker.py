1630fb23dc66dede669d90f3f868e5bbd3e0bcd2
lgc20 <luogaochen@gmail.com>
2021-06-20 12:34:50 -0400

    stable version of dqn_maker.py and replay_buffer.py

--- dqn_maker.py
+++ dqn_maker.py
@@ -1,14 +1,20 @@
 from tensorflow.python import keras
 import tensorflow as tf
+import config
 
 
-def dqn_maker(n_actions):
-    dqn = keras.Sequential([
-        keras.layers.Dense(10, input_dim=2, activation='tanh'),
-        keras.layers.Dense(10, activation='tanh'),
-        keras.layers.Dense(10, activation='tanh'),
-        keras.layers.Dense(n_actions)
-    ])
-    dqn.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer='adam')
+def dqn_maker():
+    state_dim = config['state_dim']
+    n_actions = config['n_actions']
+    activation = config['nn_activation']
+    dimension = config['nn_dimension']
 
-    return dqn
+    model = keras.Sequential()
+    model.add(tf.keras.Input(dimension=state_dim))
+    for i in dimension:
+        model.add(tf.keras.layers.Dense(i, activation=activation))
+    model.add(tf.keras.layers.Dense(n_actions))
+
+    model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer='adam')
+
+    return model
--- replay_buffer.py
+++ replay_buffer.py
@@ -1,6 +1,7 @@
 import os
 import ray
 import numpy as np
+import config
 
 
 @ray.remote
@@ -8,29 +9,21 @@ class ReplayBuffer:
     """Replay Buffer to store transitions.
     This implementation was heavily inspired by Fabio M. Graetz's replay buffer
     here: https://github.com/fg91/Deep-Q-Learning/blob/master/DQN.ipynb"""
-    def __init__(self, size=1000000, input_shape=(1, 2), use_per=True, offset=0.1):
-        """
-        Arguments:
-            size: Integer, Number of stored transitions
-            input_shape: Shape of the preprocessed state
-            use_per: Use PER instead of classic experience replay
-        """
-        self.size = size
-        self.input_shape = input_shape
+    def __init__(self):
+        state_dim = config['state_dim']
+        self.use_per = config['use_per']
+        self.offset = config['offset']
+        self.size = config['buffer_size']
+        self.input_shape = (state_dim,)
         self.count = 0  # total index of memory written to, always less than self.size
         self.current = 0  # index to write to
 
         # Pre-allocate memory
-        self.actions = np.empty(self.size, dtype=np.int32)
+        self.actions = np.empty(self.size, dtype=np.uint8)
         self.rewards = np.empty(self.size, dtype=np.float32)
         self.terminal_flags = np.empty(self.size, dtype=np.bool)
-        self.states = np.empty((self.size, 2), dtype=np.uint8)
-        if use_per:
-            self.priorities = np.full(self.size, fill_value=offset, dtype=np.float32)
-        else:
-            self.priorities = np.empty(self.size, dtype=np.float32)
-        self.use_per = use_per
-        self.offset = offset
+        self.states = np.empty((self.size, state_dim), dtype=np.uint8)
+        self.priorities = np.zeros(self.size, dtype=np.float32)
 
     def add_experience(self, action, state, reward, terminal):
         """Saves a transition to the replay buffer
@@ -49,22 +42,20 @@ class ReplayBuffer:
         self.states[self.current, ...] = state
         self.rewards[self.current] = reward
         self.terminal_flags[self.current] = terminal
-        self.priorities[self.current] = max(np.amax(self.priorities), 1)  # make the most recent experience important
+        self.priorities[self.current] = max(np.amax(self.priorities), 1.0)  # make the most recent experience important
         self.count = max(self.count, self.current+1)
         self.current = (self.current + 1) % self.size
 
-    def get_minibatch(self, batch_size=32, priority_scale=0.0):
-        """Returns a minibatch of self.batch_size = 32 transitions
-        Arguments:
-            batch_size: How many samples to return
-            priority_scale: How much to weight priorities. 0 = completely random, 1 = completely based on priority
+    def get_minibatch(self):
+        """
         Returns:
             A tuple of states, actions, rewards, new_states, and terminals
             If use_per is True:
                 An array describing the importance of transition. Used for scaling gradient steps.
                 An array of each index that was sampled
         """
-
+        batch_size = config['batch_size']
+        priority_scale = config['priority_scale']
         if self.count - 1 < batch_size:
             raise ValueError('Not enough memories to get a minibatch')
 
@@ -94,7 +85,6 @@ class ReplayBuffer:
         Arguments:
             indices: Indices to update
             errors: For each index, the error between the target Q-vals and the predicted Q-vals
-            offset: base priority scale
         """
         for i, e in zip(indices, errors):
             self.priorities[i] = abs(e) + self.offset
